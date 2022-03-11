a645084f0e35c6136087a57f478f78ebedd06775
bdoug <bbadougl@gmail.com>
2022-03-11 14:28:29 -0500

    commiting lab 1 as test

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+       // for (int i = 0; i < 10; i++) {
+       //     System.out.println(i);
+       //  }
+	int x = 0;
+	while (x < 10) {
+		System.out.println(x);
+		x += 1;
+	}
+    }
+}
